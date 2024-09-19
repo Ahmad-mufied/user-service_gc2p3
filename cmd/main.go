dfffc719004f049d5167ca76300b0ea18e512c3b
Ahmad-mufied <teranggelap01@gmail.com>
2024-09-19 13:20:18 +0700

    fix: gRPC port

--- cmd/main.go
+++ cmd/main.go
@@ -45,7 +45,7 @@ func main() {
 	pb.RegisterAuthUserServiceServer(grpcServer, userServer)
 	reflection.Register(grpcServer)
 
-	gRPCPort := config.Viper.GetString("GRPC_PORT")
+	gRPCPort := config.Viper.GetString("PORT")
 	listen, err := net.Listen("tcp", fmt.Sprintf(":%s", gRPCPort))
 	if err != nil {
 		log.Fatal(err)
